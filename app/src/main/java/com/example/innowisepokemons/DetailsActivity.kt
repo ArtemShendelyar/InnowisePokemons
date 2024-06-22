74de043430ac9fe1288ac3a9d75ce6701b2cb5d9
ArtemShendelyar <JuniorRashCat@gmail.com>
2024-06-23 01:41:26 +0300

    Война с git. Часть первая. Я проиграл.

--- app/src/main/java/com/example/innowisepokemons/DetailsActivity.kt
+++ app/src/main/java/com/example/innowisepokemons/DetailsActivity.kt
@@ -1,8 +1,10 @@
 package com.example.innowisepokemons
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
+import androidx.core.view.updateLayoutParams
 import com.example.innowisepokemons.databinding.ActivityPokemonDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
@@ -14,27 +16,42 @@ class DetailsActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val pokemonId = intent.extras?.getInt("pokemon_id", -1) ?: -1
-        if (pokemonId != -1) {
-            val pokemon = PokemonData.getPokemonById(pokemonId)
-            if (pokemon != null) {
-                binding.apply {
-                    pokemonImage.setImageResource(pokemon.picture)
-                    pokemonName.text = pokemon.name
-                    pokemonWeight.text = pokemon.weight.toString()
-                    pokemonHeight.text = pokemon.height.toString()
-                    pokemonTypes.text = pokemon.elementalType.toString()
-                    backBtn.setOnClickListener { finish() }
-                }
-            } else {
-                showErrorAndFinish("Такого покемона не существует :с")
+        with(binding) {
+            if (pokemonId == -1) {
+                Toast.makeText(this@DetailsActivity, "Плохой ID покемона.", Toast.LENGTH_SHORT).show()
+                finish()
+                return@with
             }
-        } else {
-            showErrorAndFinish("Такого ID нет!")
+            PokemonData.getPokemonById(pokemonId)?.let { pokemon ->
+                pokemonImage.setImageResource(pokemon.picture)
+                pokemonName.text = pokemon.name
+                pokemonWeight.text = "Вес: " + pokemon.weight.toString()
+                pokemonHeight.text = "Рост: " + pokemon.height.toString()
+                pokemonTypes.text = "Типы: " + pokemon.elementalType.toString()
+                backBtn.setOnClickListener { finish() }
+            } ?: showErrorAndFinish("Такого покемона нет. Увы.")
         }
     }
 
     private fun showErrorAndFinish(errorMessage: String) {
-        Toast.makeText(this, "Упс. Что-то сломалось.", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
         finish()
+    }
+    fun resizePokemon(view: View) {
+        // Я мог бы использовать тут биндинг. Но тогда мне легче было бы задать обработчик
+        // через анонимную функцию в методе DetailsActivity onCreate.
+        // Но пока я захотел протестировать код на различные варианты реализации.
+        if (view.layoutParams.height + view.layoutParams.width > 1600){
+            view.updateLayoutParams {
+                this.height = 0
+                this.width = 0
+            }
+        }else{
+            view.updateLayoutParams {
+                this.height += 200
+                this.width += 200
+            }
+
+        }
     }
 }
